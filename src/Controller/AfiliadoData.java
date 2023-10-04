378cbc65f982bd102ae0b47733ea166f699079d1
Laboratorio-01 <gnulinuxoffline@gmail.com>
2023-10-04 18:04:57 -0300

    Se agregan métodos faltante a Clase AfiliadoData

--- src/Controller/AfiliadoData.java
+++ src/Controller/AfiliadoData.java
@@ -8,6 +8,11 @@ import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 import java.sql.SQLSyntaxErrorException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AfiliadoData {
 
@@ -41,11 +46,88 @@ public class AfiliadoData {
             JOptionPane.showMessageDialog(null, "Registro duplicado: " + integrity.getMessage());
         } catch (SQLSyntaxErrorException syn) {
             JOptionPane.showMessageDialog(null, "Error de Sintaxis en sentencia SQL:\n " + syn.getMessage());
-        } catch (Exception e) {
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "No se puede acceder a Afiliados: " + e.getMessage());
         }
     }
-    
 
+    public void eliminarAfiliado(int id) {
+        String sql = "UPDATE afiliados SET estado=0 WHERE idAfiliado=?;";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            if (esActivo(id)) {
+                ps.setInt(1, id);
+                int exito = ps.executeUpdate();
+                if (exito > 0) {
+                    JOptionPane.showMessageDialog(null, "El afiliado se eliminó correctamente");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "El afiliado ya se encuentra dado de baja.");
+            }
+        } catch (SQLSyntaxErrorException syn) {
+            JOptionPane.showMessageDialog(null, "Error de Sintaxis en sentencia SQL:\n " + syn.getMessage());
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "No se puede acceder a Afiliados: " + e.getMessage());
+        }
+    }
+
+    public void actualizarAfiliado(Afiliado afiliado) {
+        String sql = "UPDATE afiliados SET nombreAfiliado=?, apellidoAfiliado=?,dni=?,domicilioAfiliado=?,telefonoAfiliado=? WHERE idAfiliado=?;";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, afiliado.getNombreAfiliado());
+            ps.setString(2, afiliado.getApellidoAfiliado());
+            ps.setString(3, afiliado.getDni());
+            ps.setString(4, afiliado.getDomicilioAfiliado());
+            ps.setString(5, afiliado.getTelefonoAfiliado());
+            ps.setInt(6, afiliado.getIdAfiliado());
+
+            int exito = ps.executeUpdate();
+
+            if (exito > 0) {
+                JOptionPane.showMessageDialog(null, "El afiliado fue actualizado.");
+            }
+        } catch (SQLSyntaxErrorException syn) {
+            JOptionPane.showMessageDialog(null, "Error de Sintaxis en sentencia SQL:\n " + syn.getMessage());
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "No se puede acceder a Afiliados: " + e.getMessage());
+        }
+    }
+
+    public List<Afiliado> listarAfiliados() {
+        List<Afiliado> listaAfiliados = new ArrayList<>();
+        String sql = "SELECT * FROM afiliados WHERE estado=1;";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    int idAfiliado = rs.getInt("idAfiliado");
+                    String nombre = rs.getString("nombreAfiliado");
+                    String apellido = rs.getString("ApellidoAfiliado");
+                    String dni = rs.getString("dni");
+                    String domicilio = rs.getString("domicilioAfiliado");
+                    String telefono = rs.getString("telefonoAfiliado");
+                    boolean estado = rs.getBoolean("estado");
+                    Afiliado afiliado = new Afiliado(idAfiliado, nombre, apellido, dni, domicilio, telefono, estado);
+                    listaAfiliados.add(afiliado);
+                }
+
+            } catch (SQLSyntaxErrorException syn) {
+                JOptionPane.showMessageDialog(null, "Error de Sintaxis en sentencia SQL:\n " + syn.getMessage());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AfiliadoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listaAfiliados;
+    }
+
+    private boolean esActivo(int idAfiliado) throws SQLException {
+        String sql = "SELECT estado FROM afiliados WHERE idAfiliado=?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, idAfiliado);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            return rs.getBoolean("estado");
+        } else {
+            return false;
+        }
+    }
 
 }
--- src/aacrhealthinsurance/AACRHealthInsurance.java
+++ src/aacrhealthinsurance/AACRHealthInsurance.java
@@ -3,6 +3,8 @@ package aacrhealthinsurance;
 
 import Controller.AfiliadoData;
 import Model.Afiliado;
+import java.util.List;
+import org.w3c.dom.ls.LSOutput;
 
 
 public class AACRHealthInsurance {
@@ -13,10 +15,26 @@ public class AACRHealthInsurance {
         
         //Prueba método Guardar
         /*Afiliado afiliado=new Afiliado("Juan", "Padilla", "25123123", "Av. Nueva 123", "11456789", true);
-        afidata.guardarAfiliado(afiliado);*/
+        afidata.guardarAfiliado(afiliado);
         
         //Prueba método Eliminar Afiliado
+        afidata.eliminarAfiliado(19);
         
+        //Prueba método actualizar
+        Afiliado newAfiliado=new Afiliado(14, "Aron", "Smith", "123", "Avenida Siempre Viva", "011123456", true);
+        afidata.actualizarAfiliado(newAfiliado);*/
+        
+        //Prueba método listar afilaidos activos
+        List<Afiliado> listaAfi=afidata.listarAfiliados();
+        for (Afiliado afiliado : listaAfi) {
+            System.out.println(afiliado.getIdAfiliado());
+            System.out.println(afiliado.getNombreAfiliado());
+            System.out.println(afiliado.getApellidoAfiliado());
+            System.out.println(afiliado.getDni());
+            System.out.println(afiliado.getDomicilioAfiliado());
+            System.out.println(afiliado.getTelefonoAfiliado());
+            System.out.println("*****************************");
+        }
         
     }
     
